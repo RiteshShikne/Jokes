68b423662250c0dbed9e11542fb3af7988c961bc
Ritesh <130898482+RiteshShikne@users.noreply.github.com>
2024-06-16 19:13:00 +0530

    Jokes

---
+++ Models/Joke.cs
@@ -0,0 +1,15 @@
+﻿namespace Jokes.Models
+{
+    public class Joke
+    {
+
+        public int id { get; set; } 
+        public string JokeQuestion{ get; set; }
+        public string JokeAnswer{ get; set; }
+
+
+        public Joke() { 
+                   
+        }
+    }
+}
